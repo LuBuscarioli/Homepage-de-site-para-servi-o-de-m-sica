fc1a617013aedf7f19cd404885c10a180fd07bb0
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2023-01-20 13:00:24 -0300

    adicionando click menu hamb

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const comecarBtn = document.querySelector('.comecar-btn')
 const comecarMenu = document.querySelector('.comecar__drop')
 const contaBtn = document.querySelector('.conta-btn')
 const contaMenu = document.querySelector('.conta__drop')
+const hambBtn = document.querySelector('.nav__hamb')
+const listaNav = document.querySelector('.nav__lista')
 
 comecarBtn.addEventListener('click', () => {
     comecarMenu.classList.toggle('active');
@@ -9,4 +11,8 @@ comecarBtn.addEventListener('click', () => {
 
 contaBtn.addEventListener('click', () => {
     contaMenu.classList.toggle('active');
+});
+
+hambBtn.addEventListener('click', () => {
+    listaNav.classList.toggle('active');
 });
